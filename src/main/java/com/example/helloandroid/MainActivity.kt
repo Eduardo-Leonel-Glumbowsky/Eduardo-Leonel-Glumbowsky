a75fde169509a5b584b37a2b00eafa3b060204f5
Eduardo-Leonel-Glumbowsky <xfirepw@gmail.com>
2025-06-23 12:19:17 -0300

    Add files via upload

---
+++ src/main/java/com/example/helloandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.helloandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
